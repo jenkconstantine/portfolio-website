4f3b8467d37f50c5fe1e8612024ed0e80d3c096a
Jenk Constantine <jenkconstantine1@gmail.com>
2018-03-03 18:59:33 -0500

    JS issues fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-console.log("Welcome to my portfolio!")
+//$(document).ready(function() {
+//  $(".project-preview").on("click", function() {
+//    $(this).siblings(".project-details").slideToggle();
+//  });
+//});
+
 $(document).ready(function() {
-  $(".project-preview").on("click", function() {
-    $(this).siblings("project-details").slideToggle();
-  });
+$(".project").on("mouseenter", function() {
+$(this).find(".project-details").fadeIn();
+});
+$(".project").on("mouseleave", function() {
+$(this).find(".project-details").fadeOut();
+});
 });
