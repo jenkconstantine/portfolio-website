6e2e4510d5d63b981d1e18e72b069baf89d3832c
Jenk Constantine <jenkconstantine1@gmail.com>
2018-03-08 13:34:39 -0500

    JS update

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@
 $(document).ready(function() {
   $(".project").on("mouseenter", function() {
     $(this).find(".project-details").fadeIn();
-  });
+  })
   $(".project").on("mouseleave", function() {
     $(this).find(".project-details").fadeOut();
-  });
-});
+  })
+})
