6f26ab17023391fb89930ed576c0e3ed2a167c20
Jenk Constantine <jenkconstantine1@gmail.com>
2018-03-07 20:05:28 -0500

    Can't figure out if Jquery is loaded, which might be causing the errors in the JS file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-//$(document).ready(function() {
+// $(document).ready(function() {
 //  $(".project-preview").on("click", function() {
 //    $(this).siblings(".project-details").slideToggle();
 //  });
-//});
+// });
 
 $(document).ready(function() {
-$(".project").on("mouseenter", function() {
-$(this).find(".project-details").fadeIn();
-});
-$(".project").on("mouseleave", function() {
-$(this).find(".project-details").fadeOut();
-});
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+  });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+  });
 });
